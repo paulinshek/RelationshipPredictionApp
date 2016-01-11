318445f190eef44c9daceb95d8e26d2f8ec1e4d4
paulinshek <paulin.shek@gmail.com>
2016-01-11 23:40:44 +0000

    Initial commit

---
+++ server.R
@@ -0,0 +1,18 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  # model to predict probabilities
+  predictProbs <- function(){
+    return(1)
+  }
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,26 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shinydashboard)
+
+dashboardPage(
+  dashboardHeader(title = "Explore our analysis here"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("See some prepopulated surveys", tabName = "prepopulated"),
+      menuItem("Answer the survey yourself!", tabName = "survey")
+      
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "prepopulated","Hi, prepopulated goes here"),
+      tabItem(tabName = "survey", "Hi, survey and results goes here")
+    )
+    
+  )
+)
